d5e7862fb166f2f98fe56df9c9a5ce50105dd627
Ofek <ofekel@ac.sce.ac.il>
2022-03-28 17:20:36 +0300

    Changed Website View

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -34,7 +34,7 @@ export default function Homepage() {
             <div className="container">
                 <div className='banner-img'></div>
                 <img src={Aviya} alt='profile image' className="profile-img"></img>                   
-                <h1 className="name">Aviya </h1>
+                <h1 className="name">Aviya David </h1>
                 <p className="description">Hi there! My name is Anna and I am a book lover, traveler and professional blogger. Follow me to stay connected!</p>
                 <button className='button'>Book Appointment</button>
             </div>
