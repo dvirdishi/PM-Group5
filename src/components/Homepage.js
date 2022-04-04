caeeb593a0972853a7495491894b5975dbc2b760
Ofek <ofekel@ac.sce.ac.il>
2022-04-04 18:17:13 +0300

    Edited Homepage

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -11,32 +11,32 @@ export default function Homepage() {
                 <div className='banner-img'></div>
                 <img src={Ofek} alt='profile image' className="profile-img"></img>                   
                 <h1 className="name">Ofek Elgozi</h1>
-                <p className="description">Hi there! My name is Anna and I am a book lover, traveler and professional blogger. Follow me to stay connected!</p>
-                <button className='button'>Book Appointment</button>
+                <p className="description">Hi there! My name is XXXX and I am a book lover, traveler and professional blogger. Follow me to stay connected!</p>
+                <button className='button'>Schedule Meeting</button>
             </div>
 
             <div className="container">
                 <div className='banner-img'></div>
                 <img src={Niran} alt='profile image' className="profile-img"></img>                   
                 <h1 className="name">Niran Dishi</h1>
-                <p className="description">Hi there! My name is Anna and I am a book lover, traveler and professional blogger. Follow me to stay connected!</p>
-                <button className='button'>Book Appointment</button>
+                <p className="description">Hi there! My name is XXXX and I am a book lover, traveler and professional blogger. Follow me to stay connected!</p>
+                <button className='button'>Schedule Meeting</button>
             </div>
 
             <div className="container">
                 <div className='banner-img'></div>
                 <img src={Dvir} alt='profile image' className="profile-img"></img>                   
                 <h1 className="name">Dvir Dishi</h1>
-                <p className="description">Hi there! My name is Anna and I am a book lover, traveler and professional blogger. Follow me to stay connected!</p>
-                <button className='button'>Book Appointment</button>
+                <p className="description">Hi there! My name is XXXX and I am a book lover, traveler and professional blogger. Follow me to stay connected!</p>
+                <button className='button'>Schedule Meeting</button>
             </div>
 
             <div className="container">
                 <div className='banner-img'></div>
                 <img src={Aviya} alt='profile image' className="profile-img"></img>                   
                 <h1 className="name">Aviya David </h1>
-                <p className="description">Hi there! My name is Anna and I am a book lover, traveler and professional blogger. Follow me to stay connected!</p>
-                <button className='button'>Book Appointment</button>
+                <p className="description">Hi there! My name is XXXX and I am a book lover, traveler and professional blogger. Follow me to stay connected!</p>
+                <button className='button'>Schedule Meeting</button>
             </div>
         </div>
     )
