dcfe60998b7a64e09160b923ec9f995f2115e894
Ofek <ofekel@ac.sce.ac.il>
2022-04-11 12:54:58 +0300

    Added Firebase File

---
+++ src/firebase.js
@@ -0,0 +1,13 @@
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBWoJ-_m01P6Q3T5xdEjyCaeH8Jbir04xE",
+  authDomain: "dona-cf141.firebaseapp.com",
+  projectId: "dona-cf141",
+  storageBucket: "dona-cf141.appspot.com",
+  messagingSenderId: "597694322136",
+  appId: "1:597694322136:web:84810d2432972dd4ac0ead"
+};
+
